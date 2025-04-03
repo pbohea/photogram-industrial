49d1f53acc9775fa534119edb17613d06c96a69e
pbohea <pbohea@gmail.com>
2025-04-03 01:28:51 +0000

    generated follow request

--- app/models/follow_request.rb
+++ app/models/follow_request.rb
@@ -19,14 +19,7 @@
 #  fk_rails_...  (recipient_id => users.id)
 #  fk_rails_...  (sender_id => users.id)
 #
-require "rails_helper"
-
-RSpec.describe FollowRequest, type: :model do
-  describe "has a belongs_to association defined called 'sender' with Class name 'User'", points: 1 do
-    it { should belong_to(:sender).class_name("User") }
-  end
-
-  describe "has a belongs_to association defined called 'recipient' with Class name 'User'", points: 1 do
-    it { should belong_to(:recipient).class_name("User") }
-  end
+class FollowRequest < ApplicationRecord
+  belongs_to :recipient, class_name: "User"
+  belongs_to :sender, class_name: "User"
 end
