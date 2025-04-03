573dae97c774958753ba9a8d2e1ef2d94ae42622
pbohea <pbohea@gmail.com>
2025-04-03 01:42:52 +0000

    added count caches

--- spec/models/follow_request_spec.rb
+++ spec/models/follow_request_spec.rb
@@ -3,7 +3,7 @@
 # Table name: follow_requests
 #
 #  id           :bigint           not null, primary key
-#  status       :string           default("pending")
+#  status       :string           default("Pending")
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
 #  recipient_id :bigint           not null
