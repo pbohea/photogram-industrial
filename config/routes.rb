63a33c8ba087e8a3576d2d37375a5f1fa3d35879
pbohea <pbohea@gmail.com>
2025-04-04 19:07:48 +0000

    Added navbar and CDN assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  root "photos#index"
+
+  devise_for :users
+  
   resources :likes
   resources :follow_requests
   resources :comments
   resources :photos
-  devise_for :users
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -16,5 +20,4 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  root "photos#index"
 end
