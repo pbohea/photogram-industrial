efd9698a8385a7753555c9f132130141d7fde845
pbohea <pbohea@gmail.com>
2025-04-05 22:27:35 +0000

    discover and feed links in navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,13 @@ class UsersController < ApplicationController
     @user = User.find_by!(username: params.fetch(:username))
   end
 
+  def feed
+    @user = User.find_by!(username: params.fetch(:username))
+  end
+
+  def discover
+    @user = User.find_by!(username: params.fetch(:username))
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,10 @@ Rails.application.routes.draw do
   resources :comments
   resources :photos
 
-  get "/:username" => "users#show", as: :user
+  get ":username" => "users#show", as: :user
   get ":username/liked" => "users#liked", as: :liked
+  get ":username/feed" => "users#feed", as: :feed
+  get ":username/discover" => "users#discover", as: :discover
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
